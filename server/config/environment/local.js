698bf884937edd5a81b735c4a8609e4bf16379ab
Alex Bannon <alexbannon@gmail.com>
2017-01-10 23:28:02 -0500

    removed environment from git repo altogether

--- server/config/environment/local.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  port:   process.env.PORT || 9000,
-  NODE_ENV: 'local',
-  POSTGRES_CONNECT: 'postgres://local:password@localhost/browserstream',
-  CACHE_TIME_IN_SECONDS: '60',
-};
