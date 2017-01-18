a67359f80cf92b2e8bc335ec74df0cbc8dd23384
Alex Bannon <alexbannon@gmail.com>
2017-01-18 00:34:28 -0500

    Added a limit, fixed pg.connect issue when passing extra param to requestStreams method

--- server/api/query/query.controller.js
+++ server/api/query/query.controller.js
@@ -4,7 +4,7 @@ var config = require('../../config/environment/local.js');
 var pg = require('pg');
 var util = require('util');
 
-var requestStreams = function(providers, sort, callback) {
+function requestStreams(providers, sort, callback) {
   var query = "SELECT * FROM provider_title JOIN provider ON provider_title.provider_id = provider.provider_id JOIN title ON title.title_id = provider_title.title_id where provider.name = '";
   for (var i = 0; i < providers.length; i++) {
     query += providers[i].name + "'"
@@ -13,8 +13,11 @@ var requestStreams = function(providers, sort, callback) {
     }
     query += " OR provider.name = '";
   }
-  query += ' ORDER BY imdb_rating DESC';
+  query += ' ORDER BY imdb_rating DESC LIMIT 15';
   pg.connect(config.POSTGRES_CONNECT, function(err, client, done) {
+    if (err) {
+      return console.error('could not connect to postgres db: ', err);
+    }
     client.query(query, function(err, result) {
       done();
       if (err) {
@@ -59,18 +62,15 @@ exports.index = function (req, res) {
       res.status(400).send('There have been validation errors: ' + util.inspect(result.array()));
       return;
     }
-    res.json({
-      hello: 'true'
+    requestStreams(req.body.providers, req.body.sort, function (err, data) {
+      if (err) {
+        res.json({ error: 'error' });
+      }
+      if (data && data.rows[0] && data.rows[0].title_name) {
+        res.header('Cache-Control', 'max-age=900, public');
+        res.json(data.rows);
+      }
     });
   });
 
-  requestStreams(req.body.providers, function (err, data) {
-    if (err) {
-      res.json({ error: 'error' });
-    }
-    if (data && data.rows[0] && data.rows[0].title_name) {
-      res.header('Cache-Control', 'max-age=900, public');
-      res.json(data.rows);
-    }
-  });
 };
