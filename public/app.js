b31b097a5b3bd58974962e766a9c289e487fe298
Alex Bannon <alexbannon@gmail.com>
2017-02-27 00:31:40 -0500

    starting to change api call based on screen size

--- public/app.js
+++ public/app.js
@@ -5,8 +5,18 @@ angular.module('browserstreams', [
   'browserstreams.home',
   'infinite-scroll',
   'ngAnimate'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+])
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $routeProvider.otherwise({redirectTo: '/'});
   $locationProvider.html5Mode(true);
+}])
+.run(['$rootScope', function($rootScope){
+  // this is to change the API request to be dependent on screen size so its not requesting 30 unnecessary titles
+  $rootScope.numFilms = 30;
+  if (window && window.screen && window.screen.width && window.screen.height) {
+    var titleWidth = Math.floor(window.screen.width / 154);
+    titleWidth = titleWidth > 12 ? 12 : titleWidth;
+    var titleHeight = Math.ceil((window.screen.height - 119) / 226);
+    $rootScope.numFilms = (titleWidth * titleHeight * 1.5);
+  }
 }]);
