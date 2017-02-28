0f457bd2a0711c24d9eb478cddfc10a1bbebcac9
Alex Bannon <alexbannon@gmail.com>
2017-02-27 20:53:49 -0500

    refactoring userSettings into service

---
+++ public/services/userSettings/userSettings.service.js
@@ -0,0 +1,50 @@
+'use strict';
+
+angular.module('browserstreams')
+
+.service('UserSettings', ['LocalStorage', function(LocalStorage) {
+  return {
+    generateUserSettings: function() {
+      var localStorageSelections = LocalStorage.getFromStorage('providerSelections');
+      localStorageSelections = localStorageSelections ? localStorageSelections.split(',') : ['netflix'];
+      var localStorageTitleTypes = LocalStorage.getFromStorage('titleTypes');
+      localStorageTitleTypes = localStorageTitleTypes ? localStorageTitleTypes.split(',') : ['movies', 'series'];
+
+      return {
+        providers: [{
+          name: 'Netflix',
+          queryName: 'netflix',
+          selected: (localStorageSelections.indexOf('netflix') > -1),
+          imageSource: '/assets/netflixLogo.webp'
+        },{
+          name: 'HBO GO',
+          queryName: 'hbo_go',
+          selected: (localStorageSelections.indexOf('hbo_go') > -1),
+          imageSource: '/assets/hboGoLogo.png',
+        },{
+          name: 'Amazon Prime',
+          queryName: 'amazon_prime',
+          selected: (localStorageSelections.indexOf('amazon_prime') > -1),
+          imageSource: '/assets/amazonPrimeLogo.webp',
+        },{
+          name: 'Hulu',
+          queryName: 'hulu',
+          selected: (localStorageSelections.indexOf('hulu') > -1),
+          imageSource: '/assets/huluLogo.png'
+        }],
+        titleType: [{
+          name: 'Movies',
+          queryName: 'movie',
+          selected: (localStorageTitleTypes.indexOf('movies') > -1)
+        }, {
+          name: 'TV Shows',
+          queryName: 'series',
+          selected: (localStorageTitleTypes.indexOf('series') > -1)
+        }],
+        sortBy: 'alphabetical',
+        sortList: ['best', 'worst', 'alphabetical'],
+        genres: []
+      };
+    }
+  };
+}]);
--- public/views/home/home.js
+++ public/views/home/home.js
@@ -9,52 +9,10 @@ angular.module('browserstreams.home', ['ngRoute', 'ngCookies'])
   });
 }])
 
-.controller('HomeCtrl', ['$scope', '$cookies', 'Modal', 'TitlesApi', 'LocalStorage', 'TitleScroll', '$rootScope', function($scope, $cookies, Modal, TitlesApi, LocalStorage, TitleScroll, $rootScope) {
+.controller('HomeCtrl', ['$scope', '$cookies', 'Modal', 'TitlesApi', 'LocalStorage', 'TitleScroll', '$rootScope', 'UserSettings', function($scope, $cookies, Modal, TitlesApi, LocalStorage, TitleScroll, $rootScope, UserSettings) {
 
-  var localStorageSelections = LocalStorage.getFromStorage('providerSelections');
-  if (localStorageSelections) {
-    localStorageSelections = localStorageSelections.split(',');
-  } else {
-    localStorageSelections = ['netflix'];
-  }
-  // TODO: break this mofo into a service
-  // TODO: download images and place in assets
-  $scope.userSettings = {
-    providers: [{
-      name: 'Netflix',
-      queryName: 'netflix',
-      selected: (localStorageSelections.indexOf('netflix') > -1),
-      imageSource: 'http://vignette4.wikia.nocookie.net/smurfs/images/a/a1/Netflix-logo.png/revision/latest?cb=20150508223333'
-    },{
-      name: 'HBO GO',
-      queryName: 'hbo_go',
-      selected: (localStorageSelections.indexOf('hbo_go') > -1),
-      imageSource: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/HBOGO.svg/2000px-HBOGO.svg.png',
-    },{
-      name: 'Amazon Prime',
-      queryName: 'amazon_prime',
-      selected: (localStorageSelections.indexOf('amazon_prime') > -1),
-      imageSource: 'http://vignette1.wikia.nocookie.net/logopedia/images/2/26/Amazon-prime.png/revision/latest?cb=20150709185638',
-    },{
-      name: 'Hulu',
-      queryName: 'hulu',
-      selected: (localStorageSelections.indexOf('hulu') > -1),
-      imageSource: 'https://assetshuluimcom-a.akamaihd.net/kitty-staging/uploads/logo_download/file/7/Hulu_Logo_Option_A.png'
-    }],
-    titleType: [{
-      name: 'Movies',
-      queryName: 'movie',
-      selected: true
-    }, {
-      name: 'TV Shows',
-      queryName: 'series',
-      selected: true
-    }],
-    sortBy: 'alphabetical',
-    sortList: ['best', 'worst', 'alphabetical'],
-    genres: []
-  };
-
+  $scope.userSettings = UserSettings.generateUserSettings();
+  
   $scope.titleScroll = new TitleScroll($scope.userSettings.sortBy, $rootScope.numFilms);
 
   $scope.displaySummary = function(titleObject) {
