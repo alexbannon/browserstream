55e52b506504c2a4a7c937ff866f0a41f6c9c3fe
Alex Bannon <alexbannon@gmail.com>
2017-02-27 20:31:03 -0500

    fixed screen size to innerWidth and height

--- public/app.js
+++ public/app.js
@@ -11,12 +11,21 @@ angular.module('browserstreams', [
   $locationProvider.html5Mode(true);
 }])
 .run(['$rootScope', function($rootScope){
-  // this is to change the API request to be dependent on screen size so its not requesting 30 unnecessary titles
+  // this is to change the API request to be dependent on screen size so its not requesting unnecessary titles
   $rootScope.numFilms = 30;
-  if (window && window.screen && window.screen.width && window.screen.height) {
-    var titleWidth = Math.floor(window.screen.width / 154);
-    var titleHeight = Math.ceil((window.screen.height - 119) / 226);
-    $rootScope.numFilms = (titleWidth * titleHeight * 1.5);
+  var width = 770, height = 1023;
+  if (window && window.innerWidth && window.innerHeight) {
+    width = window.innerWidth;
+    height = window.innerHeight;
+  } else if (window && window.screen && window.screen.width && window.screen.height) {
+    width = window.screen.width;
+    height = window.screen.height;
   }
+  var titleWidth = Math.floor(width / 154);
+  var titleHeight = Math.ceil((height - 119) / 226);
+  $rootScope.numFilms = (titleWidth * titleHeight * 1.5);
+
   $rootScope.numFilms = $rootScope.numFilms >= 120 ? 120 : $rootScope.numFilms;
+  // round up to tens place to reduce clutter in caching system
+  $rootScope.numFilms = (Math.ceil($rootScope.numFilms / 10) * 10);
 }]);
