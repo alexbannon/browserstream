48587db34c798d4e336342c858741ddc2f799d2e
Alex Bannon <alexbannon@gmail.com>
2017-03-12 13:40:36 -0400

    added directive to handle broken images

---
+++ public/directives/errSrc/errSrc.directive.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('browserstreams')
+
+.directive('errSrc', ['$compile', function($compile) {
+  return {
+    scope: {
+      replacedTitle: '@'
+    },
+    link: function(scope, element) {
+      element.bind('error', function() {
+        var e = $compile('<div class="titleImageContainer">{{replacedTitle}}</div>')(scope);
+        element.replaceWith(e);
+      });
+    }
+  };
+}]);
